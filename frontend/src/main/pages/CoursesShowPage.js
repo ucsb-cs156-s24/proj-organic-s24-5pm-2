484ca52548f1e2c94e490ca1b0a1731b6b7a86e7
Abhishek Ambastha <abhishekambastha@Abhisheks-MacBook-Pro.local>
2024-05-26 02:49:50 -0700

    made changes to the course show page file

--- frontend/src/main/pages/CoursesShowPage.js
+++ frontend/src/main/pages/CoursesShowPage.js
@@ -1,29 +1,34 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend';
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import CoursesTable from 'main/components/Courses/CoursesTable';
-import { useBackend } from 'main/utils/useBackend';
-import { useCurrentUser } from 'main/utils/currentUser';
+import { useCurrentUser} from 'main/utils/currentUser';
+import {useParams} from "react-router-dom";
 
 export default function CoursesShowPage() {
-    const { id } = useParams();
+    let { id } = useParams();
     const { data: currentUser } = useCurrentUser();
 
-    const { data: course, error: _error, status: _status } = useBackend(
-        // Stryker disable next-line all : don't test internal caching of React Query
-        [`/api/courses/${id}`],
-        // Stryker disable next-line all : GET is the default
-        { method: "GET", url: `/api/courses/${id}` },
-        []
-    );
-    const courses = course ? [course] : [];
+    const { data: courses, error: _error, status: _status } =
+        useBackend(
+            // Stryker disable next-line all : don't test internal caching of React Query
+            [`/api/courses?id=${id}`],
+
+            { // Stryker disable next-line all : GET is the default
+                method: "GET", url: "/api/courses/get",
+                params: {
+                    id
+                },
+            },
+    []
+        );
 
     return (
         <BasicLayout>
             <div className="pt-2">
-                <h1>Course Details</h1>
+                <h1>Course</h1>
                 <CoursesTable courses={courses} currentUser={currentUser} />
             </div>
         </BasicLayout>
-    );
+    )
 }
