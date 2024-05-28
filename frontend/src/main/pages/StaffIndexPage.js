6101f8a510b80bbd6bf9dcb9bddf91882629909d
Richard Fang <richardfang888@gmail.com>
2024-05-27 18:37:00 -0700

    rf- fixed some links on index page

--- frontend/src/main/pages/StaffIndexPage.js
+++ frontend/src/main/pages/StaffIndexPage.js
@@ -7,8 +7,8 @@ export default function StaffIndexPage() {
     <BasicLayout>
       <div className="pt-2">
         <h1>Index page not yet implemented</h1>
-        <p><a href="/placeholder/create">Create</a></p>
-        <p><a href="/placeholder/edit/1">Edit</a></p>
+        <p><a href="/staff/create">Create</a></p>
+        <p><a href="/staff/edit/1">Edit</a></p>
       </div>
     </BasicLayout>
   )
