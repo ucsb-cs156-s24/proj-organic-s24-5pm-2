370d433fdb7df83311c390ce630a7f32ed1da420
Richard Fang <richardfang888@gmail.com>
2024-05-28 00:50:00 -0700

    rf- edited index page links

--- frontend/src/main/pages/StaffIndexPage.js
+++ frontend/src/main/pages/StaffIndexPage.js
@@ -1,14 +1,17 @@
+import React from 'react';
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from 'react-router-dom';
 
 export default function StaffIndexPage() {
+  const { courseId } = useParams();
 
   // Stryker disable all : placeholder for future implementation
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Index page not yet implemented</h1>
-        <p><a href="/staff/create">Create</a></p>
-        <p><a href="/staff/edit/1">Edit</a></p>
+        <p><a href={`/courses/${courseId}/staff/create`}>Create</a></p>
+        <p><a href={`/courses/${courseId}/staff/edit/1`}>Edit</a></p>
       </div>
     </BasicLayout>
   )
