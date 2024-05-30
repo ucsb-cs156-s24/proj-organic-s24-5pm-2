bd32114f3bbe026b04534b07e65e4960d1a8e5d5
SloggyYoggy <adrumm825@gmail.com>
2024-05-30 02:06:47 -0700

    making contact page file

---
+++ frontend/src/main/pages/ContactUsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { Link } from "react-router-dom"; 
+
+export default function ContactUsPage() {
+    return (
+        <BasicLayout>
+            <div className="text-center pt-5">
+                <h1>contact info here.</h1>
+            </div>
+        </BasicLayout>
+    );
+}
